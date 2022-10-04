9098ebad8cea0fd981eccadafa7f295f88460a30
leonardoespina <leonardoespina@hotmail.com>
2022-10-04 05:21:59 -0400

    INICIO CALE API

---
+++ app/config/db.config.js
@@ -0,0 +1,36 @@
+const env = require("./env.js");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(env.database, env.username, env.password, {
+  host: env.host,
+  dialect: env.dialect,
+  //operatorsAliases: false,
+
+  pool: {
+    max: env.max,
+    min: env.pool.min,
+    acquire: env.pool.acquire,
+    idle: env.pool.idle,
+  },
+});
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+//Models/Tables
+db.users = require("../model/user.model.js")(sequelize, Sequelize);
+db.TipoUsuarios = require("../model/tipousuario.model.js")(
+  sequelize,
+  Sequelize
+);
+
+//db.users.hasOne(db.TipoUsuario);
+
+/*db.TipoUsuarios.hasMany(db.users, {
+  foreignKey: "id",
+});*/
+db.TipoUsuarios.hasOne(db.users, { foreignKey: "idUser" });
+db.users.belongsTo(db.TipoUsuarios, { foreignKey: "idUser" });
+module.exports = db;
+//db.users.hasOne(db.TipoUsuario);
+/*db.TipoUsuarios.hasMany(db.users, { foreignKey: "idUser" });
+db.users.belongsTo(db.TipoUsuarios, { foreignKey: "idUser" });*/
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// api routes
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Our App." });
+});
+
+require("./app/route/user.route.js")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
